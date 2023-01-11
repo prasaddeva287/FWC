bd10917d9501063c34f0e36fbf5f7e3206390707
prasaddeva287 <113084945+prasaddeva287@users.noreply.github.com>
2023-01-11 16:54:55 +0530

    Add files via upload

--- LINE/11/10.2/2/codes/line.py
+++ LINE/11/10.2/2/codes/line.py
@@ -12,19 +12,14 @@ def line_gen(A,B):
      x_AB[:,i]= temp1.T
    return x_AB
 
-#Input parameters
-x_1=np.array(([-4]))
-y_1=np.array(([3]))
+#input values
+p=np.array(([-4,3]))
 m=1/2
-c=np.array(([-5]))
-#Formula of an 
-c=((m*x_1-y_1)/(2))
 
-print("c=", c)
-
-x = np.linspace(-5,5,100)
+x = np.linspace(-4,3,100)
 y = 0.5*x-5
 plt.plot(x, y, '-r', label='y=0.5x-5')
+vert_labels = ['x-2y+10']
 plt.title('Graph of x-2y+10=0')
 plt.xlabel('x', color='#1C2833')
 plt.ylabel('y', color='#1C2833')
