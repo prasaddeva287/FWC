0fbd085d8e11461ac439a7b8420e1c2a635c68c9
prasaddeva287 <113084945+prasaddeva287@users.noreply.github.com>
2023-01-19 11:52:39 +0530

    Add files via upload

--- LINE/11/10.2/2/codes/line.py
+++ LINE/11/10.2/2/codes/line.py
@@ -1,28 +1,62 @@
+
 #Python libraries for math and graphics
 import numpy as np
+import mpmath as mp
+import math as ma
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
-def line_gen(A,B):
-   len =10
-   dim = A.shape[0]
-   x_AB = np.zeros((dim,len))
-   lam_1 = np.linspace(0,1,len)
-   for i in range(len):
-     temp1 = A + lam_1[i]*(B-A)
-     x_AB[:,i]= temp1.T
-   return x_AB
 
-#input values
-p=np.array(([-4,3]))
-m=1/2
 
-x = np.linspace(-4,3,100)
-y = 0.5*x-5
-plt.plot(x, y, label='y=0.5x-5')
-vert_labels = ['p(-4,3)']
-plt.title('Graph of x-2y+10=0')
-plt.xlabel('x', color='#1C2833')
-plt.ylabel('y', color='#1C2833')
-plt.legend(loc='upper left')
-plt.grid()
-plt.show()
+#if using termux
+import subprocess
+import shlex
+#end if
+
+def line_dir_pt(m,A,k1,k2):
+  len = 10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(k1,k2,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*m
+    x_AB[:,i]= temp1.T
+  return x_AB       
+
+
+#Input parameters
+P=  np.array(([-4,3]))
+
+
+#Direction vector
+m=np.array(([1,1/2]))                                                              
+z=np.array(([0,1],[-1,0]))                           
+n=z@m                                     
+
+##Generating the line 
+k1=-8
+k2=3
+x_mP = line_dir_pt(m,P,k1,k2)
+
+
+#Plotting all lines
+plt.plot(x_mP[0,:],x_mP[1,:],label='Line equation')
+
+#Labeling the coordinates
+tri_coords = np.vstack((P,)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['P']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+
+plt.show()               
+
