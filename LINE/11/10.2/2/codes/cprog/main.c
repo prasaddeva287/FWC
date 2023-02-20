d925d0dea840d8fc007c6a5ff5e9c3b6d04de499
prasaddeva287 <113084945+prasaddeva287@users.noreply.github.com>
2023-02-20 15:31:05 +0530

    Add files via upload

--- LINE/11/10.2/2/codes/cprog/main.c
+++ LINE/11/10.2/2/codes/cprog/main.c
@@ -12,17 +12,17 @@ https://github.com/prasaddeva287/FWC/blob/main/LICENSE*/
 //main Functions
 int main()                 
 {
-	double **P,**omat,**m,**n;        //initializing the variables
-	double slope=0.5;                    // initializing the slope
+	double **P,**omat,**m,**n,**c;        //initializing the variables                   
 	int M=2,N=1;                    // initializing rows and colums
 	P=loadtxt("a.dat",2,1);		//loading the point A from the text file
-    
-	m=dirvec(slope);                // Directional vector m
-	print(m,M,N);                   //printing Directional vector
-	save(m,M,N); 			//saving the result to the figure
+	m=loadtxt("m.dat",2,1);                // Directional vector m
+	//save(m,M,N);                    //saving the result to the figure
 	omat=loadtxt("omat.dat",2,2);   //loading the point omat from the text file
-	n=matmul(omat,m,2,2,2);		//Matrix multiplication of two matrices
-	print(n,M,N);			//printing the normal vector
-	
+	n=matmul(omat,m,2,2,2);
+        print(n,M,N);
+	save(n,M,N);
+	c=matmul(m,P,2,2,2);
+	printf("The value of c is%lf",c[0][0]);
+        
 }
 
