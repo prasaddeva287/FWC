7d8811ac7efd65c8d69bf64d04be2f29674cecdd
prasaddeva287 <113084945+prasaddeva287@users.noreply.github.com>
2023-02-20 15:36:34 +0530

    Update main.c

--- LINE/11/10.2/2/codes/cprog/main.c
+++ LINE/11/10.2/2/codes/cprog/main.c
@@ -12,16 +12,21 @@ https://github.com/prasaddeva287/FWC/blob/main/LICENSE*/
 //main Functions
 int main()                 
 {
-	double **P,**omat,**m,**n,**c;        //initializing the variables                   
-	int M=2,N=1;                    // initializing rows and colums
-	P=loadtxt("a.dat",2,1);		//loading the point A from the text file
-	m=loadtxt("m.dat",2,1);                // Directional vector m
-	//save(m,M,N);                    //saving the result to the figure
-	omat=loadtxt("omat.dat",2,2);   //loading the point omat from the text file
+	double **P,**omat,**m,**n,**c;         //initializing the variables                   
+	int M=2,N=1;                           // initializing rows and colums
+	P=loadtxt("a.dat",2,1);		       //loading the point A from the text file
+	m=loadtxt("m.dat",2,1);                // loading Directional vector m
+	omat=loadtxt("omat.dat",2,2);          //loading the point omat from the text file
+	
+	// Calculate the matrix multiplication
 	n=matmul(omat,m,2,2,2);
-        print(n,M,N);
-	save(n,M,N);
-	c=matmul(m,P,2,2,2);
+	//print the value
+        print(n,M,N);                  
+	//save the result
+	save(n,M,N);                    
+	// The calculation for the value of c
+	c=matmul(m,P,2,2,2);   
+	//print the result
 	printf("The value of c is%lf",c[0][0]);
         
 }
